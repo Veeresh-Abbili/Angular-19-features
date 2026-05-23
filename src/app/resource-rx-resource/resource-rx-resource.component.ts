e94bf5a1d02e24b7e0462b98e9ca4fd5bd87e4eb
Veeresh-Abbili <abbiliveeresh98@gmail.com>
2026-05-23 17:46:32 +0530

    Initial commit

---
+++ src/app/resource-rx-resource/resource-rx-resource.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit,resource } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Resource } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-rx-resource',
+  imports: [],
+  templateUrl: './resource-rx-resource.component.html',
+  styleUrl: './resource-rx-resource.component.css'
+})
+export class ResourceRxResourceComponent implements OnInit {
+
+  constructor(private http:HttpClient){}
+
+  userData:any[]=[];
+
+  Url="https://jsonplaceholder.typicode.com/users"
+
+ngOnInit(): void {
+ this.getData();
+}
+
+
+  getData(){
+     return this.http.get<any>(this.Url).subscribe((res:any)=>{
+         this.userData = res;
+    })
+  }
+
+    // rxResource API:
+
+    //  rxResourceData = rxResource({
+    //     loader: ()=> this.http.get<any>(this.Url)
+    //  }
+    //  )
+
+    //  resource API:
+
+      resource = resource({
+        loader:() => fetch(this.Url).then(res=>res.json() as Promise<any>)
+      })
+
+}
