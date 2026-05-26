f9350d0678ed5e79b8fe38a505d2ca9fae986f1f
Veeresh-Abbili <abbiliveeresh98@gmail.com>
2026-05-26 16:56:09 +0530

    linked lignal updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,13 @@
-import { Component } from '@angular/core';
-import { HydrationComponent } from './hydration/hydration.component';
-import { LinkedSignalComponent } from "./linked-signal/linked-signal.component";
-import { ResourceRxResourceComponent } from './resource-rx-resource/resource-rx-resource.component';
-import { RouterOutlet } from '@angular/router';
-import { StandaloneComponent } from './standalone/standalone.component';
-import { CommonModule } from '@angular/common';
+import { Component,} from '@angular/core';
+import { LinkedSignalComponent } from './linked-signal/linked-signal.component';
+
 @Component({
   selector: 'app-root',
-  standalone:true,
-  imports: [RouterOutlet, StandaloneComponent, CommonModule],
-  imports: [ResourceRxResourceComponent],
-  imports: [HydrationComponent, LinkedSignalComponent],
+  imports: [LinkedSignalComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular19-features';
-
-     isLogin:boolean = false;
-
-     login(){
-
-     this.isLogin = true;
-     console.log("standalone components works")
-    }
-
-
-
-
-
-
-
-
-
 
 }
--- src/app/linked-signal/linked-signal.component.ts
+++ src/app/linked-signal/linked-signal.component.ts
@@ -8,15 +8,41 @@ import { Component, linkedSignal, signal } from '@angular/core';
 })
 export class LinkedSignalComponent {
 
-sourceSignal = signal(0);
-updateSignal = linkedSignal({
-  source: this.sourceSignal,
-  computation:()=>this.sourceSignal()*5
-});
+// sourceSignal = signal(0);
+// updateSignal = linkedSignal({
+//   source: this.sourceSignal,
+//   computation:()=>this.sourceSignal()*5
+// });
 
-updateSourceSignal(){
-  this.sourceSignal.set(2);
-  this.updateSignal.set(10);
-}
+// updateSourceSignal(){
+//   this.sourceSignal.set(2);
+//   this.updateSignal.set(10);
+// }
+
+ //  currentEmail = signal({id:1,name:"FED"});
+  //  alternateEmail = computed(()=>`${this.currentEmail().name}@gmail.com`);
+
+  //  alternateEmailId(){
+  //    this.currentEmail.set({id:2,name:"FEDLearing"});
+  //  }
+
+
+  // currentEmail = signal({id:1,name:"FED"});
+  //  alternateEmail = linkedSignal(()=>`${this.currentEmail().name}@gmail.com`);
+
+  //  alternateEmailId(){
+  //    this.currentEmail.set({id:2,name:"FEDLearing"});
+  //  }
+
+   currentEmail = signal({id:1,name:"FED"});
+   alternateEmail = linkedSignal({
+    source:this.currentEmail,
+    computation:()=>`${this.currentEmail().name}@gmail.com`,
+    equal:(a:any,b:any ) => a.id !== b.id 
+   });
+
+   alternateEmailId(){
+     this.currentEmail.set({id:2,name:"FEDLearing"});
+   }
 
 }
